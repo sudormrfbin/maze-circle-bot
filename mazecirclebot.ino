399f7c65354d4a6495376e76afb6a8ea4d6f5dda
Gokul Soumya <gokulps15@gmail.com>
2022-10-19 21:17:43 +0530

    refator

--- mazecirclebot.ino
+++ mazecirclebot.ino
@@ -7,23 +7,39 @@
 #define rightIR A1
 #define rightMostIR A0
 
-AF_DCMotor motor1(1, MOTOR12_1KHZ); 
+#define ON_WHITE 0
+#define ON_BLACK 1
+
+AF_DCMotor motor1(1, MOTOR12_1KHZ);
 AF_DCMotor motor2(2, MOTOR12_1KHZ);
 
-void left_turn() {
+void turn_left() {
   motor1.setSpeed(100);
   motor2.setSpeed(100);
   motor1.run(FORWARD);
   motor2.run(BACKWARD);
 }
 
-void right_turn() {
+void turn_right() {
   motor1.setSpeed(100);
   motor2.setSpeed(100);
   motor1.run(BACKWARD);
   motor2.run(FORWARD);
 }
 
+void go_straight() {
+  motor1.setSpeed(100);
+  motor2.setSpeed(100);
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+}
+
+void stop_moving() {
+  motor1.setSpeed(0);
+  motor2.setSpeed(0);
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+}
 
 void setup() {
   pinMode(leftMostIR, INPUT);
@@ -32,46 +48,21 @@ void setup() {
   pinMode(rightIR, INPUT);
   pinMode(rightMostIR, INPUT);
 
-  delay(1000); // 1s delay to do positioning adjustments
+  delay(1000);  // 1s delay to do positioning adjustments
 }
 
-void loop(){
-  //line detected by both
-  if(digitalRead(leftIR)==0 && digitalRead(rightIR)==0){
-    //Forward
-    motor1.run(FORWARD);
-    motor1.setSpeed(100);
-    motor2.run(FORWARD);
-    motor2.setSpeed(100);
-
+void loop() {
+  int left = digitalRead(leftIR);
+  int right = digitalRead(rightIR);
+  if (left == ON_WHITE && right == ON_WHITE) {
+    go_straight();
+  } else if (left == ON_WHITE && right == ON_BLACK) {
+    turn_left();
+  } else if (left == ON_BLACK && right == ON_WHITE) {
+    turn_right();
+  } else if (left == ON_BLACK && right == ON_BLACK) {
+    stop_moving();
   }
-  //line detected by left sensor
-  else if(digitalRead(leftIR)==0 && !analogRead(rightIR)==0){
-    //turn left
-    motor1.run(FORWARD);
-    motor1.setSpeed(100);
-    motor2.run(BACKWARD);
-    motor2.setSpeed(100);
-
-
-  }
-  //line detected by right sensor
-  else if(!digitalRead(leftIR)==0 && digitalRead(rightIR)==0){
-    //turn right
-    motor1.run(BACKWARD);
-    motor1.setSpeed(100);
-    motor2.run(FORWARD);
-    motor2.setSpeed(100);
-
-  }
-  //line detected by none
-  else if(!digitalRead(leftIR)==0 && !digitalRead(rightIR)==0){
-    //stop
-    motor1.run(RELEASE);
-    motor1.setSpeed(0);
-    motor2.run(RELEASE);
-    motor2.setSpeed(0);
-
-  }
-
 }
+
+
